d9ef2a4c7730812a09349d13c3ea31f150376375
Daniel <danieltsy890@gmail.com>
2023-01-03 13:22:29 +0800

    Restructured project files

--- app/src/main/java/my/edu/tarc/tarcvote/ui/RegisterActivity.kt
+++ app/src/main/java/my/edu/tarc/tarcvote/ui/RegisterActivity.kt
@@ -18,18 +18,80 @@ import my.edu.tarc.tarcvote.R
 
 class RegisterActivity : AppCompatActivity(){
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         supportActionBar?.hide()
 
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
 
+
+        val button: Button = findViewById(R.id.btnSigUp)
+        val name: TextInputEditText = findViewById(R.id.regName)
+        val email: TextInputEditText = findViewById(R.id.regEmail)
+        val password: TextInputEditText = findViewById(R.id.regPassword)
+        val cPassword: TextInputEditText = findViewById(R.id.regCPassword)
+        var isValid = false
+
+
+
+        button.setOnClickListener{
+            val regName = name.text.toString()
+            val regEmail = email.text.toString()
+            val regPassword = password.text.toString()
+            val regCPassword = cPassword.text.toString()
+
+
+            if(regName.isEmpty() || regEmail.isEmpty()|| regPassword.isEmpty() || regCPassword.isEmpty()){
+                if(regName.isEmpty()){
+                    name.error = "Please enter your name"
+                }
+                if(regEmail.isEmpty()){
+                    email.error = "Please enter your email"
+                }
+                if(regPassword.isEmpty()){
+                    password.error = "Please enter your password"
+                }
+                if(regCPassword.isEmpty()){
+                    cPassword.error = "Please re-enter your password"
+                }
+                Toast.makeText(this@RegisterActivity, "Please enter your info details", Toast.LENGTH_SHORT).show()
+            }else if(!regEmail.endsWith("student.tarc.edu.my") && !regEmail.endsWith("student.tarc.edu.my")){
+                email.error = "Please enter your TARC email"
+                Toast.makeText(this@RegisterActivity, "Please enter your TARC email",Toast.LENGTH_SHORT).show()
+            }else if (regPassword.length < 6){
+                password.error = "Password must be at least at least 6 characters long"
+                Toast.makeText(this@RegisterActivity, "Password must be at least 6 characters long",Toast.LENGTH_SHORT).show()
+            }else if (!regPassword.any { it.isUpperCase()}){
+                password.error = "Password must contain at least one uppercase character"
+                Toast.makeText(this@RegisterActivity, "Password must contain at least one uppercase character",Toast.LENGTH_SHORT).show()
+            }else if (!regPassword.any { it.isDigit()}) {
+                password.error = "Password must contain at least one digit"
+                Toast.makeText(this@RegisterActivity, "Password must contain at least one digit",Toast.LENGTH_SHORT).show()
+            }else if (isValid) {
+
+                auth.createUserWithEmailAndPassword(regEmail, regPassword).addOnCompleteListener {
+                    val users = hashMapOf(
+                        "name" to regName,
+                        "email" to regEmail,
+                        "uid" to auth.currentUser!!.uid
+                    )
+
+                }
+            }
+
+
+
+
+        }
     }
-
-
-
-
 }
+
+
+
+
 
 
 
