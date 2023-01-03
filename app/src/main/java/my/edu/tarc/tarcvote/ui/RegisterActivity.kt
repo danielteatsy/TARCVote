f22ea168e770f13a218e09864190edfc301f9606
Daniel <danieltsy890@gmail.com>
2023-01-03 18:53:31 +0800

    Restructured project files

--- app/src/main/java/my/edu/tarc/tarcvote/ui/RegisterActivity.kt
+++ app/src/main/java/my/edu/tarc/tarcvote/ui/RegisterActivity.kt
@@ -45,56 +45,77 @@ class RegisterActivity : AppCompatActivity(){
             startActivity(intent)
         }
 
-
-
-        button.setOnClickListener{
+        button.setOnClickListener {
             val regName = name.text.toString()
             val regEmail = email.text.toString()
             val regPassword = password.text.toString()
             val regCPassword = cPassword.text.toString()
 
-
-            if(regName.isEmpty() || regEmail.isEmpty()|| regPassword.isEmpty() || regCPassword.isEmpty()){
-                if(regName.isEmpty()){
+            // Check if any of the input fields are empty
+            if (regName.isEmpty() || regEmail.isEmpty() || regPassword.isEmpty() || regCPassword.isEmpty()) {
+                if (regName.isEmpty()) {
                     name.error = "Please enter your name"
                 }
-                if(regEmail.isEmpty()){
+                if (regEmail.isEmpty()) {
                     email.error = "Please enter your email"
                 }
-                if(regPassword.isEmpty()){
+                if (regPassword.isEmpty()) {
                     password.error = "Please enter your password"
                 }
-                if(regCPassword.isEmpty()){
+                if (regCPassword.isEmpty()) {
                     cPassword.error = "Please re-enter your password"
                 }
                 Toast.makeText(this@RegisterActivity, "Please enter your info details", Toast.LENGTH_SHORT).show()
-            }else if(!regEmail.endsWith("student.tarc.edu.my") && !regEmail.endsWith("student.tarc.edu.my")){
-                email.error = "Please enter your TARC email"
-                Toast.makeText(this@RegisterActivity, "Please enter your TARC email",Toast.LENGTH_SHORT).show()
-            }else if (regPassword.length < 6){
-                password.error = "Password must be at least at least 6 characters long"
-                Toast.makeText(this@RegisterActivity, "Password must be at least 6 characters long",Toast.LENGTH_SHORT).show()
-            }else if (!regPassword.any { it.isUpperCase()}){
-                password.error = "Password must contain at least one uppercase character"
-                Toast.makeText(this@RegisterActivity, "Password must contain at least one uppercase character",Toast.LENGTH_SHORT).show()
-            }else if (!regPassword.any { it.isDigit()}) {
-                password.error = "Password must contain at least one digit"
-                Toast.makeText(this@RegisterActivity, "Password must contain at least one digit",Toast.LENGTH_SHORT).show()
-            }else if (isValid) {
+                return@setOnClickListener
+            }
 
-                auth.createUserWithEmailAndPassword(regEmail, regPassword).addOnCompleteListener { it ->
-                    if (it.isSuccessful) {
-                        // Send verification email
-                        auth.currentUser?.sendEmailVerification()
-                            ?.addOnCompleteListener {
-                                if (it.isSuccessful) {
-                                    // Email sent
-                                    Log.d(TAG, "Verification email sent to ${auth.currentUser?.email}")
-                                } else {
-                                    // Email not sent
-                                    Log.e(TAG, "Error sending verification email", it.exception)
-                                }
+            // Check if the email is a valid TARC email
+            if (!regEmail.endsWith("tarc.edu.my") && !regEmail.endsWith("student.tarc.edu.my")) {
+                email.error = "Please enter your TARC email"
+                Toast.makeText(this@RegisterActivity, "Please enter your TARC email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Check if the password is at least 6 characters long
+            if (regPassword.length < 6) {
+                password.error = "Password must be at least 6 characters long"
+                Toast.makeText(this@RegisterActivity, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }// Check if the password contains at least one uppercase character
+            if (!regPassword.any { it.isUpperCase() }) {
+                password.error = "Password must contain at least one uppercase character"
+                Toast.makeText(this@RegisterActivity, "Password must contain at least one uppercase character", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Check if the password contains at least one digit
+            if (!regPassword.any { it.isDigit() }) {
+                password.error = "Password must contain at least one digit"
+                Toast.makeText(this@RegisterActivity, "Password must contain at least one digit", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Check if the password and confirm password fields match
+            if (regPassword != regCPassword) {
+                cPassword.error = "Passwords do not match"
+                Toast.makeText(this@RegisterActivity, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // If all checks pass, create the user
+            auth.createUserWithEmailAndPassword(regEmail, regPassword).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Send verification email
+                    auth.currentUser?.sendEmailVerification()
+                        ?.addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                // Email sent
+                                Log.d(TAG, "Verification email sent to ${auth.currentUser?.email}")
+                            } else {
+                                // Email not sent
+                                Log.e(TAG, "Error sending verification email", it.exception)
                             }
+                        }
 
                     val user = hashMapOf(
                         "name" to regName,
@@ -102,34 +123,32 @@ class RegisterActivity : AppCompatActivity(){
                         "uid" to auth.currentUser!!.uid
                     )
 
-                        if (regEmail.endsWith("tarc.edu.my")) {
-                            // User is an admin
-                            user["isUser"] = "0"
-                        } else if (regEmail.endsWith("student.tarc.edu.my")) {
-                            // User is a user
-                            user["isUser"] = "1"
-                        } else {
-                            // Invalid email domain
-                            Toast.makeText(this@RegisterActivity, "Error: Invalid email domain", Toast.LENGTH_SHORT).show()
-                        }
-                        db.collection("users").document(auth.currentUser!!.uid).set(user).addOnSuccessListener {
-                            Toast.makeText(this@RegisterActivity, "Account Created", Toast.LENGTH_SHORT).show()
-                            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-                            startActivity(intent)
-                        }
-                            .addOnFailureListener { e ->
-                                Toast.makeText(this@RegisterActivity, "Something went wrong, try again", Toast.LENGTH_SHORT).show()
-                            }
-
+                    if (regEmail.endsWith("tarc.edu.my")) {
+                        // User is an admin
+                        user["isUser"] = "0"
+                    } else if (regEmail.endsWith("student.tarc.edu.my")) {
+                        // User is a user
+                        user["isUser"] = "1"
+                    } else {
+                        // Invalid email domain
+                        Toast.makeText(this@RegisterActivity, "Error: Invalid email domain", Toast.LENGTH_SHORT).show()
+                        return@addOnCompleteListener
+                    }
+                    db.collection("users").document(auth.currentUser!!.uid).set(user).addOnSuccessListener {
+                        Toast.makeText(this@RegisterActivity, "Account Created", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
                 } else {
-                        Toast.makeText(this, "Something went wrong, try again", Toast.LENGTH_SHORT).show()
+                    // User creation failed
+                    Toast.makeText(this@RegisterActivity, "Error: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                 }
             }
-
-        } else {
-                Toast.makeText(this, "Something went wrong, try again", Toast.LENGTH_SHORT).show()
-            }
         }
+
+
+
+
     }
 }
 
